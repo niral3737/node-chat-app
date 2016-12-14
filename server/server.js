65cf7bf50e1a1b1a0e1616ae9b372d265e0a6950
niral.eagle100@gmail.com <niral.eagle100@gmail.com>
2016-12-14 22:16:38 +0530

    init commit

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@
  * Created by niral on 14/12/16.
  */
 'use strict'
-let path = require('path');
-let express = require('express');
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
 let app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('Express app is up and running on port 3000');
+app.listen(port, () => {
+    console.log(`App is up and running on ${port}`);
 });
